a5d088a0e130c9376b6f3fb96bee056a3d9a8b28
ahmed.habib <ahmed.habib@sewedy.com.eg>
2025-08-06 15:03:17 +0300

    Finished Splash Screen

---
+++ app/src/main/java/com/ahmed/group/deliveryshebin/utils/DateManager.kt
@@ -0,0 +1,43 @@
+package com.ahmed.group.deliveryshebin.utils
+
+import android.os.Build
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+
+
+fun getCurrentDate() = Calendar.getInstance().getTime().toString()
+
+fun String.parseDate(): Date {
+    val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSXXX", Locale.getDefault())
+    return format.parse(this) ?: throw IllegalArgumentException("Invalid date format")
+}
+
+fun String.formatDate(): String {
+    var date: String
+
+    when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
+            LocalDateTime.parse(this)
+            val parsedDate = LocalDateTime.parse(this)
+            val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+            date = parsedDate.format(formatter)
+        }
+
+        else -> {
+            try {
+                val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS", Locale.ENGLISH)
+                val parsedDate = inputFormat.parse(this)
+                val outputFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+                date = parsedDate?.let { outputFormat.format(it) } ?: ""
+            } catch (_: Exception) {
+                date = ""
+            }
+        }
+    }
+
+    return date
+}
--- app/src/main/java/com/ahmed/group/deliveryshebin/utils/Extensions.kt
+++ app/src/main/java/com/ahmed/group/deliveryshebin/utils/Extensions.kt
@@ -12,19 +12,9 @@ import android.provider.Settings
 import android.util.Patterns
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
 import java.io.Serializable
-import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.Calendar
-import java.util.Date
 import java.util.Locale
 import java.util.UUID
-
-fun Context.showToast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
 
 fun String.isValidEmail() =
     this.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
@@ -68,47 +58,9 @@ inline fun <reified T : Serializable?> Bundle.serializable(key: String): T? = wh
     else -> @Suppress("DEPRECATION") getSerializable(key) as? T
 }
 
-fun String.isArabic(): Boolean {
-    val arabicRegex = "[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF]".toRegex()
-    return arabicRegex.containsMatchIn(this)
-}
-
 fun Context.openSettings() {
     val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
         data = Uri.fromParts("package", packageName, null)
     }
     startActivity(intent)
-}
-
-fun getCurrentDate() = Calendar.getInstance().getTime().toString()
-
-fun String.parseDate(): Date {
-    val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSXXX", Locale.getDefault())
-    return format.parse(this) ?: throw IllegalArgumentException("Invalid date format")
-}
-
-fun String.formatDate(): String {
-    var date: String
-
-    when {
-        Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
-            LocalDateTime.parse(this)
-            val parsedDate = LocalDateTime.parse(this)
-            val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
-            date = parsedDate.format(formatter)
-        }
-
-        else -> {
-            try {
-                val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS", Locale.ENGLISH)
-                val parsedDate = inputFormat.parse(this)
-                val outputFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
-                date = parsedDate?.let { outputFormat.format(it) } ?: ""
-            } catch (_: Exception) {
-                date = ""
-            }
-        }
-    }
-
-    return date
 }
